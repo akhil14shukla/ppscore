76a54a91ead6d315b8840cd88402568757a928fa
akhil14shukla <akhil14shukla@gmail.com>
2021-07-10 13:40:35 +0530

    Added PPS

--- ppscore.py
+++ ppscore.py
@@ -12,7 +12,7 @@ from sklearn import utils
 
 
 def pps(df,categorical_features=None,numerical_features=None):
-    columns = ["Feature", "Target", "Type of Prediction", "Cross-Val Score", "Training Score", "Model"]
+    columns = ["Feature", "Target","PPS", "Type of Prediction", "Cross-Val Score", "Training Score","Naive-Baseline Score", "Model"]
     pps_df=pd.DataFrame(columns=columns)
     for i in df.columns:
         for j in df.columns:
@@ -22,17 +22,26 @@ def pps(df,categorical_features=None,numerical_features=None):
                 label_encoded_y = np.array(lab_enc.fit_transform(df[j])).reshape(-1,1)
                 x_train,x_test,y_train,y_test = train_test_split(np.array(df[i]).reshape(-1,1),label_encoded_y)
                 model.fit(x_train,y_train)
+                y_pred=model.predict(x_test)
+                f1 = metrics.f1_score(y_test,y_pred)
+                mode = np.full((len(x_test),1),df[j].mode())
+                f1_naive = metrics.f1_score(y_test,mode)
+                pps_score=(f1-f1_naive)/(1 - f1_naive)
                 cv_score=model.score(x_test,y_test)
                 train_score=model.score(x_train,y_train)
-                pps_df = pps_df.append({"Feature":i,"Target":j, "Type of Prediction":"Classification", "Cross-Val Score":cv_score, "Training Score":train_score, "Model":"DecisionTreeClassifier()"},ignore_index=True)
+                pps_df = pps_df.append({"Feature":i,"Target":j,"PPS":pps_score,"Type of Prediction":"Classification","Cross-Val Score":cv_score,"Training Score":train_score,"Naive-Baseline Score":f1_naive,"Model":"DecisionTreeClassifier()"},ignore_index=True)
             else:
                 model = DecisionTreeRegressor()
                 # lab_enc = preprocessing.LabelEncoder()
                 # label_encoded_y = np.array(lab_enc.fit_transform(df[j])).reshape(-1,1)
                 x_train,x_test,y_train,y_test = train_test_split(np.array(df[i]).reshape(-1,1),np.array(df[j]).reshape(-1,1))
                 model.fit(x_train,y_train)
-                cv_score=model.score(x_test,y_test)
+                median = np.full((len(x_test),1),df[j].median())
+                naive_mae = metrics.mean_absolute_error(y_test, median)
+                y_pred=model.predict(x_test)
+                mae=metrics.mean_absolute_error(y_test,y_pred)
                 train_score=model.score(x_train,y_train)
-                pps_df = pps_df.append({"Feature":i,"Target":j, "Type of Prediction":"Regression", "Cross-Val Score":cv_score, "Training Score":train_score, "Model":"DecisionTreeRegressor()"},ignore_index=True)
+                pps_score = 1 - mae/naive_mae
+                pps_df=pps_df.append({"Feature":i,"Target":j,"PPS":pps_score,"Type of Prediction":"Regression","Cross-Val Score":cv_score,"Training Score":train_score,"Naive-Baseline Score":naive_mae,"Model":"DecisionTreeRegressor()"},ignore_index=True)
     
     return pps_df
