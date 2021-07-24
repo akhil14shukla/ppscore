f31b3eab0b0c49c3be29a2d29f3de22ed59f9043
akhil14shukla <akhil14shukla@gmail.com>
2021-07-24 11:23:32 +0530

    Added library on PyPi

--- setup.py
+++ setup.py
@@ -2,18 +2,20 @@ from setuptools import find_packages, setup
 
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
-    
-    
+
+requirements = ["pandas>=1.0.0,<2.0.0", "scikit-learn >= 0.20.2, < 1.0.0"]
+
 setup(
     name='PyCustom',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.0.3',
     description='Customised Python Functions',
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/akhil14shukla/PyCustom",
     author='Akhil Shukla (akhil14shukla)',
     license='GNU v3.0',
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
