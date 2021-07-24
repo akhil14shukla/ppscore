f89043c2365d65d0dadb1d4bf40be5e40d51ab1a
akhil14shukla <akhil14shukla@gmail.com>
2021-07-24 10:38:03 +0530

    Created Librarywq

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='PyCustom',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Customised Python Functions',
+    author='Akhil Shukla (akhil14shukla)',
+    license='GNU v3.0',
+)
